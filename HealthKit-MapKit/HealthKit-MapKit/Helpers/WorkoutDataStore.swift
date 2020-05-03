4894962ed41680e0a4e69db87092d9d0bbff7636
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-05-03 18:04:26 -0500

    Add models + privacy to plist

---
+++ HealthKit-MapKit/HealthKit-MapKit/Helpers/WorkoutDataStore.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutDataStore.swift
+//  HealthKit-MapKit
+//
+//  Created by Alex Rhodes on 5/3/20.
+//  Copyright © 2020 Alex Rhodes. All rights reserved.
+//
+
+import HealthKit
+
+class WorkoutDataStore {
+  
+  class func save(distanceWorkout: DistanceWorkout,
+                  completion: @escaping ((Bool, Error?) -> Swift.Void)) {
+    
+  }
+  
+}
