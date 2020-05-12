05cf0a9381a13276dd3ec2dde46454fd37d5d619
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-05-11 21:46:16 -0500

    add color extension + update UI

---
+++ HealthKit-MapKit/HealthKit-MapKit/Custom Views/ARAnimatedButton.swift
@@ -0,0 +1,9 @@
+//
+//  ARAnimatedButton.swift
+//  HealthKit-MapKit
+//
+//  Created by Alex Rhodes on 5/11/20.
+//  Copyright © 2020 Alex Rhodes. All rights reserved.
+//
+
+import Foundation
---
+++ HealthKit-MapKit/HealthKit-MapKit/Helpers/UIColorExtention.swift
@@ -0,0 +1,20 @@
+//
+//  UIColorExtention.swift
+//  HealthKit-MapKit
+//
+//  Created by Alex Rhodes on 5/11/20.
+//  Copyright © 2020 Alex Rhodes. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+  public class var graidentGray1: UIColor {
+        return #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+    }
+    
+    public class var graidentGray2: UIColor {
+           return #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+       }
+}
